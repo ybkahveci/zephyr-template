824d78d9db43bc8a1bb2810a4688c4aa8a218bee
Yusuf Bunyamin Kahveci <ybkahveci@gmail.com>
2023-11-03 09:18:36 +0100

    dat_sensor: add initial application files

---
+++ applications/dat_sensor/src/main.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/sensor.h>
+#include <app_version.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
+
+int main(void)
+{
+
+	printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
+
+	return 0;
+}
+
